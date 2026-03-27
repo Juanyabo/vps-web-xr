954cf8a0ea46d1c27416acb7d7ab5ec7ec1ab7c8
JuanGianella <juan.gianella19@gmail.com>
2026-03-27 14:57:41 +0100

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/vps-web-xr",
+  resolve: {
+    preserveSymlinks: true, // important for local `file:` or `npm link` packages
+  },
+  optimizeDeps: {
+    include: [
+      '@multisetai/vps',
+      '@multisetai/vps/core',
+      '@multisetai/vps/webxr',
+      'react',
+      'react-dom',
+      'three',
+    ],
+  },
+  server: {
+    open: true,
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
+  },
+});
+
